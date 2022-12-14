9bc284d2f0401011ae7af99dfa50cec69ed61f44
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-14 22:49:18 +0300

    Add migration for users database

---
+++ src/Helpdesk.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Helpdesk.WebApp.Data;
+
+public class ApplicationDbContext : IdentityDbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+    }
+}
