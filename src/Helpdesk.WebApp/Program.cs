dba607b31ed7adcb81b742f22d2c6eeb206a5d10
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-14 23:00:20 +0300

    Update used services

--- src/Helpdesk.WebApp/Program.cs
+++ src/Helpdesk.WebApp/Program.cs
@@ -1,4 +1,7 @@
-using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Helpdesk.WebApp.Data;
 
 namespace Helpdesk.WebApp;
 
@@ -7,34 +10,43 @@ public class Program {
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found!");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-        builder.Services.AddControllers();
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        builder.Services.AddControllersWithViews();
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
-        builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-            .AddNegotiate();
-
-        builder.Services.AddAuthorization(options => {
-            // By default, all incoming requests will be authorized according
-            // to the default policy
-            options.FallbackPolicy = options.DefaultPolicy;
-        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline
         if (app.Environment.IsDevelopment()) {
+            app.UseMigrationsEndPoint();
             app.UseSwagger();
             app.UseSwaggerUI();
+        } else {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
         }
 
         app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-        app.UseAuthentication();
+        app.UseRouting();
+
         app.UseAuthorization();
 
-        app.MapControllers();
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
 
         app.Run();
     }
