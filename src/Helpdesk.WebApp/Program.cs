f36fccde9a6cc99d3d3056643cf95ef106b130b8
acidicMercury8 <acidicmercury8@outlook.com>
2023-02-16 19:54:15 +0300

    Switch to use top-level statements

--- src/Helpdesk.WebApp/Program.cs
+++ src/Helpdesk.WebApp/Program.cs
@@ -3,55 +3,49 @@ using Microsoft.EntityFrameworkCore;
 
 using Helpdesk.WebApp.Models;
 
-namespace Helpdesk.WebApp;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program {
-    public static void Main(string[] args) {
-        var builder = WebApplication.CreateBuilder(args);
+// Add services to the container
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=\"" +
+        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
+        "\\Helpdesk.db\""));
 
-        // Add services to the container
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite("Data Source=\"" +
-                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) +
-                "\\Helpdesk.db\""));
+builder.Services.AddControllersWithViews();
 
-        builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate();
 
-        builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
-            .AddNegotiate();
+builder.Services.AddAuthorization(options => {
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
-        builder.Services.AddAuthorization(options => {
-            // By default, all incoming requests will be authorized according to the default policy
-            options.FallbackPolicy = options.DefaultPolicy;
-        });
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline
-        if (app.Environment.IsDevelopment()) {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        } else {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.UseStatusCodePagesWithRedirects("/Error/");
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+} else {
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseStatusCodePagesWithRedirects("/Error/");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
