c0e31b38c6d849b783d4cf4a81c8f6d72799fd81
acidicMercury8 <acidicmercury8@outlook.com>
2023-02-17 17:03:29 +0300

    Add conrollers and views for basic CRUD operations

---
+++ src/Helpdesk.WebApp/Controllers/CabinetController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Helpdesk.WebApp.Controllers;
+
+[Route("[controller]")]
+public class CabinetController : Controller {
+    [HttpGet]
+    [Route("")]
+    public IActionResult Index() {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}")]
+    public IActionResult Details(int id) {
+        return View();
+    }
+
+    [HttpGet]
+    [Route("[action]")]
+    public IActionResult Create() {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(IFormCollection collection) {
+        try {
+            return RedirectToAction(nameof(Index));
+        } catch {
+            return View();
+        }
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}")]
+    public IActionResult Edit(int id) {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]/{id}")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, IFormCollection collection) {
+        try {
+            return RedirectToAction(nameof(Index));
+        } catch {
+            return View();
+        }
+    }
+
+    [HttpGet]
+    [Route("[action]/{id}")]
+    public IActionResult Delete(int id) {
+        return View();
+    }
+
+    [HttpPost]
+    [Route("[action]/{id}")]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id, IFormCollection collection) {
+        try {
+            return RedirectToAction(nameof(Index));
+        } catch {
+            return View();
+        }
+    }
+}
