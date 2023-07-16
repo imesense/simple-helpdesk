6e65d69a8ccfe1388879d9e14cff8c7a01ad77a1
acidicMercury8 <acidicmercury8@outlook.com>
2023-07-16 15:53:54 +0300

    Rewrite database context tests

---
+++ src/Helpdesk.WebApp.Tests/Models/DbContextIntegrationTests.cs
@@ -0,0 +1,109 @@
+using Microsoft.EntityFrameworkCore;
+
+using Helpdesk.WebApp.Models;
+
+namespace Helpdesk.WebApp.Tests.Models;
+
+[TestClass]
+public class DbContextIntegrationTests {
+    private ApplicationDbContext _context = null!;
+
+    [TestInitialize]
+    public void Initialize() {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        _context = new ApplicationDbContext(options);
+    }
+
+    [TestCleanup]
+    public void Cleanup() {
+        _context.Dispose();
+    }
+
+    [TestMethod]
+    public void ApplicationDbContext_ShouldCreateDatabase() {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "CreatedDatabase")
+            .Options;
+        var context = new ApplicationDbContext(options);
+        Assert.IsNotNull(context);
+
+    }
+
+    [TestMethod]
+    public void ApplicationDbContext_ShouldConnectDatabase() {
+        Assert.IsTrue(_context.Database.CanConnect());
+    }
+
+    [TestMethod]
+    public void Locations_ShouldNotBeNull() {
+        Assert.IsNotNull(_context.Locations);
+    }
+
+    [TestMethod]
+    public void Locations_ShouldNotBeEmpty() {
+        _context.Locations.Add(new Location {
+            Name = "Test Location",
+        });
+        _context.SaveChanges();
+        Assert.AreEqual(1, _context.Locations.Count());
+    }
+
+    [TestMethod]
+    public void Cabinets_ShouldNotBeNull() {
+        Assert.IsNotNull(_context.Cabinets);
+    }
+
+    [TestMethod]
+    public void Cabinets_ShouldNotBeEmpty() {
+        _context.Cabinets.Add(new Cabinet {
+            Number = "125",
+        });
+        _context.SaveChanges();
+        Assert.AreEqual(1, _context.Cabinets.Count());
+    }
+
+    [TestMethod]
+    public void Users_ShouldNotBeNull() {
+        Assert.IsNotNull(_context.Users);
+    }
+
+    [TestMethod]
+    public void Users_ShouldNotBeEmpty() {
+        _context.Users.Add(new User {
+            Name = "Name",
+            Phone = "+7 999 000 11 22",
+        });
+        _context.SaveChanges();
+        Assert.AreEqual(1, _context.Users.Count());
+    }
+
+    [TestMethod]
+    public void Statuses_ShouldNotBeNull() {
+        Assert.IsNotNull(_context.Statuses);
+    }
+
+    [TestMethod]
+    public void Statuses_ShouldNotBeEmpty() {
+        _context.Statuses.Add(new Status {
+            Description = "Description",
+        });
+        _context.SaveChanges();
+        Assert.AreEqual(1, _context.Statuses.Count());
+    }
+
+    [TestMethod]
+    public void Tickets_ShouldNotBeNull() {
+        Assert.IsNotNull(_context.Tickets);
+    }
+
+    [TestMethod]
+    public void Tickets_ShouldNotBeEmpty() {
+        _context.Tickets.Add(new Ticket {
+            Description = "Description",
+        });
+        _context.SaveChanges();
+        Assert.AreEqual(1, _context.Tickets.Count());
+    }
+}
---
+++ src/Helpdesk.WebApp.Tests/Models/DbContextMockTests.cs
@@ -0,0 +1,82 @@
+using Moq;
+
+using MockQueryable.Moq;
+
+using Helpdesk.WebApp.Models;
+
+namespace Helpdesk.WebApp.Tests.Models;
+
+[TestClass]
+public class DbContextMockTests {
+    private class TestService {
+        public TestService(IDbContext context) => _ = context;
+    }
+
+    private Mock<IDbContext> _contextMock = null!;
+
+    [TestInitialize]
+    public void Initialize() {
+        _contextMock = new Mock<IDbContext>();
+    }
+
+    [TestMethod]
+    public void DbContext_CanBeCreated() {
+        var service = new TestService(Mock.Of<IDbContext>());
+        Assert.IsNotNull(service);
+    }
+
+    [TestMethod]
+    public void Locations_MatchesExpectedValue() {
+        var locations = new List<Location> {
+            new Location(),
+        };
+        var mockDbSet = locations.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Locations).Returns(mockDbSet.Object);
+        CollectionAssert.AreEquivalent(locations,
+            _contextMock.Object.Locations.ToList());
+    }
+
+    [TestMethod]
+    public void Cabinets_MatchesExpectedValue() {
+        var cabinets = new List<Cabinet> {
+            new Cabinet(),
+        };
+        var mockDbSet = cabinets.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Cabinets).Returns(mockDbSet.Object);
+        CollectionAssert.AreEquivalent(cabinets,
+            _contextMock.Object.Cabinets.ToList());
+    }
+
+    [TestMethod]
+    public void Users_MatchesExpectedValue() {
+        var uses = new List<User> {
+            new User(),
+        };
+        var mockDbSet = uses.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Users).Returns(mockDbSet.Object);
+        CollectionAssert.AreEquivalent(uses,
+            _contextMock.Object.Users.ToList());
+    }
+
+    [TestMethod]
+    public void Statuses_MatchesExpectedValue() {
+        var statuses = new List<Status> {
+            new Status(),
+        };
+        var mockDbSet = statuses.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Statuses).Returns(mockDbSet.Object);
+        CollectionAssert.AreEquivalent(statuses,
+            _contextMock.Object.Statuses.ToList());
+    }
+
+    [TestMethod]
+    public void Tickets_MatchesExpectedValue() {
+        var tickets = new List<Ticket> {
+            new Ticket(),
+        };
+        var mockDbSet = tickets.AsQueryable().BuildMockDbSet();
+        _contextMock.Setup(x => x.Tickets).Returns(mockDbSet.Object);
+        CollectionAssert.AreEquivalent(tickets,
+            _contextMock.Object.Tickets.ToList());
+    }
+}
